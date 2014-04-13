a1e751012f608779ad4c4080b82125bf363eb23d
Patrick <pevargas90@gmail.com>
2014-04-13 09:32:47 -0600

    Moved Queertionary into it's own repo.

--- queertionary/models/term.js
+++
@@ -1,9 +0,0 @@
-// Term SCchema
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
-
-var TermSchema = new Schema({
-  word : String
-});
-
-module.exports = mongoose.model( 'Term', TermSchema );
